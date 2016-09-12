097f1ac5b992b94e57fd26450b3439be7432e792
Naomi <naocol_97@hotmail.com>
2016-09-12 18:37:14 -0400

    tarea 5

---
+++ js/script.js
@@ -0,0 +1,17 @@
+(function(){
+	console.log('Hola Mundo');
+})();
+
+var descripcion = '';
+var cantidad = 0;
+
+var array = [];
+array.push('Hola Mundo');
+array.push('JavaScript');
+console.log(array);
+
+var objeto = {}
+
+objeto.cantidad=10;
+objeto.descripcion='Descripcion';
+console.log(objeto);
